f6922153c714d987a290767663cdfbd18a7fd685
lafont <chaster_killer@hotmail.fr>
2018-03-01 21:54:16 +0100

    correction conversion en str

--- outputProblem.py
+++ outputProblem.py
@@ -7,5 +7,7 @@ def problemToStdOut (pb : Problem) -> None:
     for vehicule in pb.vehicules:
         print(len(vehicule.rides), end='')
         for ride in vehicule.rides: 
-            print(" " + ride.number, end='')
+            print(" " + str(ride.number), end='')
         print()
+
+

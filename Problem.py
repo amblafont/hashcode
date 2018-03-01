bb08da4192a6cfcd812eb8ecfb05628873baf477
Raphaël L <raphlight0@gmail.com>
2018-03-01 21:19:06 +0100

    adds missing self's and completes make vehicules move

--- Problem.py
+++ Problem.py
@@ -14,23 +14,43 @@ class Problem:
         self.maxTime = maxTime
         self.bonus = bonus
 
-    def LaunchSimulation() -> None:
+    def LaunchSimulation(self) -> None:
         MapVehiculesRides()
         MakeVehiculesMove()
         
-    def GetInactiveVehicules() -> List[Vehicule]:
+    def GetInactiveVehicules(self) -> List[Vehicule]:
     	inactiveVehicules = []
     	for vehicule in self.vehicules:
     		if (not vehicule.isActive):
     			inactiveVehicules.append(vehicule)
     	return inactiveVehicules
 
-    def MapVehiculesRides() -> None:
+    def MapVehiculesRides(self) -> None:
     	for i in range(len(self.rides, 0)):
     		if self.rides[i].status != RideStatus.available:
     			self.rides.remove(ride)
 
     	vehicules = GetInactiveVehicules()
 
-    def MakeVehiculesMove() -> None:
-        return
+    def MakeVehiculesMove(self) -> None:
+        for vehicule in self.vehicules:
+            if vehicule.rides:
+                ride = vehicule.rides[-1]
+
+                # voiture déjà sur le trajet
+                if vehicule.isActive:
+                    if vehicule.position.r > ride.position.r:
+                        vehicule.position.r -= 1
+                    elif vehicule.position.r < ride.position.r:
+                        vehicule.position.r += 1
+                    elif vehicule.position.c > ride.position.c:
+                        vehicule.position.c -= 1
+                    elif vehicule.position.c < ride.position.c:
+                        vehicule.position.c +=1
+                    else:
+                        vehicule.isActive = False
+                        
+                # voiture attend de pouvoir faire le trajet
+                elif ride.available:
+                    if ride.position == vehicule.position:
+                        vehicule.isActive = True

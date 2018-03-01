f77e2b1c27615481ded16ac35306b9b29d8ac9bf
Raphaël L <raphlight0@gmail.com>
2018-03-01 20:43:59 +0100

    add score function to vehicles

--- Vehicule.py
+++ Vehicule.py
@@ -1,4 +1,5 @@
 from typing import List,Type
+from math import *
 import Point,Ride
 class Vehicule:
 
@@ -7,3 +8,9 @@ class Vehicule:
         self.position = position
         self.rides = rides
         self.isActive = False;
+
+    # Plus le score est proche de zero, plus il est bon
+    def score(currentStep: int, ride: Ride) -> int:
+        distance = math.fabs(self.position.r - ride.startPoint.r) + math.fabs(self.position.c - ride.startPoint.c)
+        earliestStart = ride.earliestTime - currentStep
+        return distance + earliestStart

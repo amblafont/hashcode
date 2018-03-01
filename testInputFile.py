b7a4e5d5123a414e22b952cf3b45586e0268ef9c
gregoire <chabedou@live.com>
2018-03-01 21:51:36 +0100

    za

--- testInputFile.py
+++ testInputFile.py
@@ -1,10 +1,10 @@
 from readInput import inputFileToProblem
 from outputProblem import problemToStdOut
-from  inputFileNames import *
+from inputFileNames import *
 
 
 
 pb = inputFileToProblem(FILE_1)
+print(pb.MapVehiculesRides())
 problemToStdOut(pb)
-
 

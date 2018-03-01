94e2f2a703fb3b5030af6eca043f362e680b0872
lafont <chaster_killer@hotmail.fr>
2018-03-01 19:46:36 +0100

    completion de la classe probleme

--- Problem.py
+++ Problem.py
@@ -3,7 +3,9 @@ import Map,Ride,Vehicule
 class Problem:
 
 
-    def __init__(self, map:Map, nbVehicules:int, rides : List[Ride.Ride]) -> None:
+    def __init__(self, map:Map, nbVehicules:int, rides : List[Ride.Ride], maxTime : int, bonus:int) -> None:
         self.map = map
         self.vehicules = [ Vehicule(Point(0,0), []) for i in range(nbVehicules)]
         self.rides = Rides
+        self.maxTime = maxTime
+        self.bonus = bonus

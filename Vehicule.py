e849e552b5700a369a4d28f82724e9f0fbbf96a5
lafont <chaster_killer@hotmail.fr>
2018-03-01 21:27:48 +0100

    oubli de self dans les méthodes

--- Vehicule.py
+++ Vehicule.py
@@ -11,15 +11,15 @@ class Vehicule:
         self.isActive = False
 
     # Plus le score est proche de zero, plus il est bon
-    def score(currentStep: int, ride: Ride) -> int:
+    def score(self, currentStep: int, ride: Ride) -> int:
         distance = math.fabs(self.position.r - ride.startPoint.r) + math.fabs(self.position.c - ride.startPoint.c)
         earliestStart = math.min(0, ride.earliestTime - currentStep)
         return distance + earliestStart
 
-    def sortRidesByScore(currentStep : int, rides : List[Ride]):
+    def sortRidesByScore(self, currentStep : int, rides : List[Ride]):
         ridesWithScore = [(ride, self.score(currentStep, ride)) for ride in rides]
         return sorted(ridesWithScore, key=lambda rideWithScore:rideWithScore[1])
-    def updateRidesByScore(currentStep : int, rides : List[Ride]) -> None:
+    def updateRidesByScore(self, currentStep : int, rides : List[Ride]) -> None:
         self.sortedRidesWithScores = self.sortRidesByScore(currentStep, rides)
 
 
